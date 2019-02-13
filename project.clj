b90c9c36ebae00a8bf11875b11913bb84d71deea
Ville Vehvil?inen <ville.vehvilainen@solita.fi>
2019-02-13 16:07:51 +0200

    Initial commit from local sources

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject lein-jib-build "0.1.1-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Apache 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
+
+  :resource-paths ["lib"]
+
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [com.google.cloud.tools/jib-core "0.1.3-SNAPSHOT-GUAVASHADOW"]]
+
+  :eval-in-leiningen true)
