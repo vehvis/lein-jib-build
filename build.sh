3db86c75ee636b946bbb744726ad3c59596a3e2e
vehvis <vvehvilainen+github@gmail.com>
2019-11-02 16:27:15 +0200

    buildscript fixes

--- build.sh
+++ build.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
 JIB_CORE_VERSION=0.12.1-SNAPSHOT-GUAVASHADOW
-# init submodule for jib
-git submodule init jib || exit 1
 
-# build jib and get the built jar
+echo "--- Checking that we have the required submodule"
+git submodule init jib || exit 1
+git submodule update jib || exit 1
+
+echo "--- Build the customised jib-core"
 cd jib
 ./gradlew :jib-core:jar || exit 1
 cd ..
+mkdir -p lib || exit 1
 cp -v jib/jib-core/build/libs/jib-core-$JIB_CORE_VERSION.jar lib/ || exit 1
 
+echo "--- Now build the plugin"
 lein do clean, install
